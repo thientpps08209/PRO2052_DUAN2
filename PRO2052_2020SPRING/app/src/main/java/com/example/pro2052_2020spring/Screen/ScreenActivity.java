1c3dbb98c5175467eb7519d6e2b568198df1f33b
thientpps08209 <45841474+thientpps08209@users.noreply.github.com>
2020-01-13 17:07:54 +0700

    upscreen man hinh chao

---
+++ PRO2052_2020SPRING/app/src/main/java/com/example/pro2052_2020spring/Screen/ScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.pro2052_2020spring.Screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pro2052_2020spring.R;
+
+public class ScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+    }
+}
